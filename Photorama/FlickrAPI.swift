5895013cfa716422687dcf878c88882df60376d8
Sergey Kozak <svkozak@icloud.com>
2018-03-12 22:19:25 -0400

    Corrected JSON error

--- Photorama/FlickrAPI.swift
+++ Photorama/FlickrAPI.swift
@@ -8,14 +8,13 @@
 
 import Foundation
 
-enum Method: String {
-	case interestingPhotos = "flickr.interestingness.getList"
-}
-
 enum FlickrError: Error {
 	case invalidJSONData
 }
 
+enum Method: String {
+	case interestingPhotos = "flickr.interestingness.getList"
+}
 
 struct FlickrAPI {
 	
@@ -28,11 +27,23 @@ struct FlickrAPI {
 		return formatter
 	}()
 	
-	private static func flickrURL (method: Method, parameters: [String: String]?) -> URL {
-		var components = URLComponents(string: baseURLString)
+	static var interestingPhotosURL: URL {
+		return flickrURL(method: .interestingPhotos,
+						 parameters: ["extras": "url_h,date_taken"])
+	}
+	
+	private static func flickrURL(method: Method,
+								  parameters: [String:String]?) -> URL {
+		var components = URLComponents(string: baseURLString)!
+		
 		var queryItems = [URLQueryItem]()
 		
-		let baseParams = ["method": method.rawValue, "format": "json", "nojsoncallback": "1", "api_key": apiKey]
+		let baseParams = [
+			"method": method.rawValue,
+			"format": "json",
+			"nojsoncallback": "1",
+			"api_key": apiKey
+		]
 		
 		for (key, value) in baseParams {
 			let item = URLQueryItem(name: key, value: value)
@@ -43,37 +54,27 @@ struct FlickrAPI {
 			for (key, value) in additionalParams {
 				let item = URLQueryItem(name: key, value: value)
 				queryItems.append(item)
-				
 			}
 		}
-		components?.queryItems = queryItems
-		return (components?.url)!
-	}
-	
-	private static func photo(fromJSON json: [String : Any]) -> Photo? {
-		guard
-			let photoID = json["id"] as? String,
-			let title = json["title"] as? String,
-			let dateString = json["dateTaken"] as? String,
-			let photoURLString = json["url_h"] as? String,
-			let url = URL(string: photoURLString),
-			let dateTaken = dateFormatter.date(from: dateString)
-			else {
-				return nil
-		}
-		return Photo(title: title, photoID: photoID, remoteURL: url, dateTaken: dateTaken)
+		components.queryItems = queryItems
 		
+		return components.url!
 	}
 	
 	static func photos(fromJSON data: Data) -> PhotosResult {
+		
 		do {
 			let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
 			
-			guard let jsonDictionary = jsonObject as? [AnyHashable : Any],
-			let photos = jsonDictionary["photos"] as? [String : Any],
-				let photosArray = photos["photo"] as? [[String : Any]] else {
+			guard
+				let jsonDictionary = jsonObject as? [AnyHashable:Any],
+				let photos = jsonDictionary["photos"] as? [String:Any],
+				let photosArray = photos["photo"] as? [[String:Any]] else {
+					
+					// The JSON structure doesn't match our expectations
 					return .failure(FlickrError.invalidJSONData)
 			}
+			
 			var finalPhotos = [Photo]()
 			for photoJSON in photosArray {
 				if let photo = photo(fromJSON: photoJSON) {
@@ -82,7 +83,8 @@ struct FlickrAPI {
 			}
 			
 			if finalPhotos.isEmpty && !photosArray.isEmpty {
-				// unable to parse any photos
+				// We weren't able to parse any of the photos.
+				// Maybe the JSON format for photos has changed.
 				return .failure(FlickrError.invalidJSONData)
 			}
 			return .success(finalPhotos)
@@ -91,10 +93,21 @@ struct FlickrAPI {
 		}
 	}
 	
-	
-	
-	static var interestingPhotosURL: URL {
-		return flickrURL(method: .interestingPhotos, parameters: ["extras": "url_h,date_taken"])
+	private static func photo(fromJSON json: [String : Any]) -> Photo? {
+		guard
+			let photoID = json["id"] as? String,
+			let title = json["title"] as? String,
+			let dateString = json["datetaken"] as? String,
+			let photoURLString = json["url_h"] as? String,
+			let url = URL(string: photoURLString),
+			let dateTaken = dateFormatter.date(from: dateString) else {
+				
+				// Don't have enough information to construct a Photo
+				return nil
+		}
+		
+		return Photo(title: title, photoID: photoID, remoteURL: url, dateTaken: dateTaken)
 	}
 	
 }
+
